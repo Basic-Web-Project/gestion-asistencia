16077e54d1da6018d533c6d61531edb774664001
Salinas Cruz Edison <salinasedisoncruz@gmail.com>
2024-03-26 10:47:40 -0500

    Prueba configuracion DB

---
+++ config/db.php
@@ -0,0 +1,18 @@
+<?php
+    class db{
+        private $host="127.0.0.1:3306";
+        private $dbname="empleado";
+        private $user="root";
+        private $pass="Ed1Sal58@";
+
+        public function conexion(){
+            try{
+                $PDO = new PDO("mysql:host=".$this->host.";dbname=".$this->dbname,$this->user,$this->pass);
+                return $PDO;
+            }catch(PDOException $e){
+                return $e->getMessage();
+            }
+        }
+    }
+
+?>
